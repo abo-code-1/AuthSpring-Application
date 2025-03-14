d809956c6d6d5f00167d2071a43f7a0ea72762ab
Abror <abrorimamsidikov@gmail.com>
2025-03-14 22:50:43 +0500

    Add files via upload

---
+++ AuthSpring-Application/src/test/java/com/example/AuthSpring_Application/AuthSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthSpring_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
